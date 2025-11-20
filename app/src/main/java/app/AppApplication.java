55b0fe60471d668a605e627383a3d79291e04f73
scavazza <renato.scavazza@vetorit.com.br>
2025-11-20 11:01:03 -0300

    teste

--- app/src/main/java/app/AppApplication.java
+++ app/src/main/java/app/AppApplication.java
@@ -3,6 +3,8 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// teste
+
 @SpringBootApplication
 public class AppApplication {
 
